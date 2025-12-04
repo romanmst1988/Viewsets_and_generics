e55ac2a93d13f308161091d790d4546274db3ae9
Роман Мстиславский <mstislavskijr@gmail.com>
2025-12-04 21:30:17 +0300

    create urls moto_2

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 
-from vehicle.views import CarViewSet, MotoCreateAPIView
+from vehicle.views import CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView, MotoUpdateAPIView, \
+    MotoDestroyAPIView
 
 app_name = VehicleConfig.name
 
@@ -11,5 +12,9 @@ router = DefaultRouter()
 router.register(r"cars", CarViewSet)
 
 urlpatterns = [
-    path("moto/create", MotoCreateAPIView.as_view(), name="moto_create"),
+    path("moto/create/", MotoCreateAPIView.as_view(), name="moto_create"),
+    path("moto/", MotoListAPIView.as_view(), name="moto_list"),
+    path("moto/<int:pk>/", MotoRetrieveAPIView.as_view(), name="moto-get"),
+    path("moto/update/<int:pk>/", MotoUpdateAPIView.as_view(), name="moto-update"),
+    path("moto/delet/<int:pk>/", MotoDestroyAPIView.as_view(), name="moto-delete"),
 ] + router.urls
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics
 
-from vehicle.models import Car
+from vehicle.models import Car, Moto
 from vehicle.serliazers import CarSerializer, MotoSerializer
 
 
@@ -10,3 +10,18 @@ class CarViewSet(viewsets.ModelViewSet):
 
 class MotoCreateAPIView(generics.CreateAPIView):
     serializer_class = MotoSerializer
+
+class MotoListAPIView(generics.ListAPIView):
+    serializer_class = MotoSerializer
+    queryset = Moto.objects.all()
+
+class MotoRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = MotoSerializer
+    queryset = Moto.objects.all()
+
+class MotoUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = MotoSerializer
+    queryset = Moto.objects.all()
+
+class MotoDestroyAPIView(generics.DestroyAPIView):
+    queryset = Moto.objects.all()
