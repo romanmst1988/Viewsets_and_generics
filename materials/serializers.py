8d72d6cd2eab8797b1ab4e66e3abdac1433c46f9
Роман Мстиславский <mstislavskijr@gmail.com>
2025-12-09 21:50:40 +0300

    add models/users Payment

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
-
 from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = ['id', 'title', 'description', 'preview', 'lessons_count']
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.conf import settings
+from materials.models import Course, Lesson
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=150, unique=False)
@@ -15,3 +16,20 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+# Добавляем модель платежей
+class Payment(models.Model):
+    PAYMENT_METHODS = (
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счёт'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Оплаченный курс')
+    lesson = models.ForeignKey(Lesson, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Оплаченный урок')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    method = models.CharField(max_length=10, choices=PAYMENT_METHODS, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f"Оплата {self.user.email} — {self.amount}"
