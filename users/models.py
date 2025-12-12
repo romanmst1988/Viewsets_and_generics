97ae1b0c7254d10829857755cde4da69b808bd76
Роман Мстиславский <mstislavskijr@gmail.com>
2025-12-12 19:50:52 +0300

    fix cod

--- users/models.py
+++ users/models.py
@@ -4,25 +4,15 @@ from django.db import models
 from django.conf import settings
 from materials.models import Course, Lesson
 
-
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=150, unique=False)
-    email = models.EmailField(unique=True)
-
-    phone = models.CharField(max_length=20, blank=True, null=True)
-    city = models.CharField(max_length=100, blank=True, null=True)
-    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
-
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
-    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты')
-    course = models.ForeignKey('materials.Course', null=True, blank=True, on_delete=models.SET_NULL,
-                               verbose_name='Оплаченный курс')
-    lesson = models.ForeignKey('materials.Lesson', null=True, blank=True, on_delete=models.SET_NULL,
-                               verbose_name='Оплаченный урок')
-    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
+
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.email
@@ -51,7 +41,7 @@ class Payment(models.Model):
         blank=True
     )
 
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
 
     PAYMENT_METHOD_CHOICES = [
         ('cash', 'Наличные'),
