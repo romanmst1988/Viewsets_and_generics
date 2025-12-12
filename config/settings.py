cee3e2eacec18de18fdef48b10f1748c0d651bdc
Роман Мстиславский <mstislavskijr@gmail.com>
2025-12-12 20:32:41 +0300

    fix cod_1

--- config/settings.py
+++ config/settings.py
@@ -123,9 +123,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Настройки JWT-токенов
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 # Настройки срока действия токенов
