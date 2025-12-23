d618395411d21ff132b26849d0099e34b417d424
Роман Мстиславский <mstislavskijr@gmail.com>
2025-12-23 18:11:13 +0300

    fix celery.py

--- config/celery.py
+++ config/celery.py
@@ -1,9 +1,10 @@
-from __future__ import absolute_import, unicode_literals
+# from __future__ import absolute_import, unicode_literals
 import os
+
 from celery import Celery
 
 # Установка переменной окружения для настроек проекта
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # Создание экземпляра объекта Celery
 app = Celery('config')
@@ -13,3 +14,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
 app.autodiscover_tasks()
+
+# Пример
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
--- config/settings.py
+++ config/settings.py
@@ -156,3 +156,20 @@ CORS_ALLOW_ALL_ORIGINS = False
 STRIPE_SECRET_KEY = "sk_test_..."
 STRIPE_SUCCESS_URL = "http://localhost:8000/success/"
 STRIPE_CANCEL_URL = "http://localhost:8000/cancel/"
+
+# Настройки для Celery
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://localhost:6379' # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Australia/Tasmania"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
